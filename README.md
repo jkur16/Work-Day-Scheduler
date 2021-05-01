bcc431554b379184a7f82ae7dd17d07d7c9c4a05
Jamie Kuras <jamiekuras@gmail.com>
2021-05-01 14:15:18 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Work-Day-Scheduler
+
+## How Day Planner works
+Uses JQuery and moment.js to setup a planner for a work day.  The planner is setup so that past hours are grey, future hours are green, and the current hour is red.  Items on the schedule can then be saved in local storage so that they can be referred to later.  They can also be deleted. 
+
+## Technologies Used
+HTML, JavaScript, JQuery, moment.js, local storage
---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+var dayDisplayEl = $('#currentDay');
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#1");
+var hour2 = $("#2");
+var hour3 = $("#3");
+var hour4 = $("#4");
+var hour5 = $("#5");
+var time = moment();
+
+// Displaying the current date
+function displayTime() {
+    var today = moment().format('dddd, MMMM Do');
+    dayDisplayEl.text(today);
+}
+setInterval(displayTime)
+
+
+// Save function
+function setPlanner() {
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
+}
+
+setPlanner();
+
+
+var saveBtn = $(".btn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+// Colors of text area change for past, present and future hours
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
